b37ba856aa307f6cd6dfa3d415222016e183c2ab
Remigijus Pankevičius <r-pankevicius@users.noreply.github.com>
2019-12-08 13:40:18 +0200

    VS UnitTesting tests for different scenarios (path combinations). Exploring expected deploy folder structures.

--- src/BusinessLogicTestsOld/FileOperationsTests.cs
+++ src/BusinessLogicTestsOld/FileOperationsTests.cs
@@ -12,15 +12,21 @@ namespace BusinessLogicTestsOld
 		[TestMethod]
 		public void NonExistingFileDoesntExist()
 		{
+			TestContext.WriteLine($"Deployment directory is {TestContext.DeploymentDirectory}");
+
 			const string filename = "😺";
-			bool exists = FileOperations.FileExists(filename);
+			string pathToFile = Path.Combine(TestContext.DeploymentDirectory, filename);
+			bool exists = FileOperations.FileExists(pathToFile);
 			Assert.IsFalse(exists, $"File '{filename} should not exist.");
 		}
 
 		[TestMethod]
 		[DeploymentItem("1-line.txt")]
+		// -> Out\1-line.txt
 		public void FileDeployedFromRoot()
 		{
+			TestContext.WriteLine($"Deployment directory is {TestContext.DeploymentDirectory}");
+
 			string fullPath = Path.Combine(
 				TestContext.DeploymentDirectory, "1-line.txt");
 			Assert.IsTrue(FileOperations.FileExists(fullPath),
@@ -29,10 +35,65 @@ namespace BusinessLogicTestsOld
 
 		[TestMethod]
 		[DeploymentItem("1-line.txt", "thedirectory")]
+		// -> Out\thedirectory\1-line.txt
 		public void FileDeployedFromRootToSpecifiedDirectory()
 		{
+			TestContext.WriteLine($"Deployment directory is {TestContext.DeploymentDirectory}");
+
 			string fullPath = Path.Combine(
 				TestContext.DeploymentDirectory, "thedirectory", "1-line.txt");
+			Assert.IsTrue(FileOperations.FileExists(fullPath),
+				$"File '{fullPath} should be deployed. Did you forget to set 'Copy if newer' property?");
+		}
+
+		[TestMethod]
+		[DeploymentItem("1-line.txt", @"thedirectory\nesteddirectory")]
+		// -> Out\thedirectory\nesteddirectory\1-line.txt
+		public void FileDeployedFromRootToSpecifiedNestedDirectory()
+		{
+			TestContext.WriteLine($"Deployment directory is {TestContext.DeploymentDirectory}");
+
+			string fullPath = Path.Combine(
+				TestContext.DeploymentDirectory, "thedirectory", "nesteddirectory", "1-line.txt");
+			Assert.IsTrue(FileOperations.FileExists(fullPath),
+				$"File '{fullPath} should be deployed. Did you forget to set 'Copy if newer' property?");
+		}
+
+		[TestMethod]
+		[DeploymentItem(@"SomeFolder\2-lines.txt")]
+		// -> Out\2-lines.txt
+		public void FileDeployedFromNestedFolder()
+		{
+			TestContext.WriteLine($"Deployment directory is {TestContext.DeploymentDirectory}");
+
+			string fullPath = Path.Combine(
+				TestContext.DeploymentDirectory, "2-lines.txt");
+			Assert.IsTrue(FileOperations.FileExists(fullPath),
+				$"File '{fullPath} should be deployed. Did you forget to set 'Copy if newer' property?");
+		}
+
+		[TestMethod]
+		[DeploymentItem(@"SomeFolder\2-lines.txt", "thedirectory")]
+		// -> Out\thedirectory\2-lines.txt
+		public void FileDeployedFromNestedFolderToSpecifiedDirectory()
+		{
+			TestContext.WriteLine($"Deployment directory is {TestContext.DeploymentDirectory}");
+
+			string fullPath = Path.Combine(
+				TestContext.DeploymentDirectory, "thedirectory", "2-lines.txt");
+			Assert.IsTrue(FileOperations.FileExists(fullPath),
+				$"File '{fullPath} should be deployed. Did you forget to set 'Copy if newer' property?");
+		}
+
+		[TestMethod]
+		[DeploymentItem(@"SomeFolder\2-lines.txt", @"thedirectory\nesteddirectory")]
+		// -> Out\thedirectory\nesteddirectory\2-lines.txt
+		public void FileDeployedFromNestedFolderToSpecifiedNestedDirectory()
+		{
+			TestContext.WriteLine($"Deployment directory is {TestContext.DeploymentDirectory}");
+
+			string fullPath = Path.Combine(
+				TestContext.DeploymentDirectory, "thedirectory", "nesteddirectory", "1-line.txt");
 			Assert.IsTrue(FileOperations.FileExists(fullPath),
 				$"File '{fullPath} should be deployed. Did you forget to set 'Copy if newer' property?");
 		}
